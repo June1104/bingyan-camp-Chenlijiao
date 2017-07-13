5f74c9977e43ec9f28edb458de8381730b9d8c81
June <1175421906qq.com>
2017-07-14 01:51:53 +0800

    do some exercise

--- maintask/myProject/src/App.vue
+++ maintask/myProject/src/App.vue
@@ -16,17 +16,20 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+    <Hello></Hello>
   </div>
 </template>
 
 <script>
+import Hello from './component/Hello.vue'
 export default {
   name: 'app',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+    components:{ Hello }
 }
 </script>
 
---
+++ maintask/myProject/src/component/Hello.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="Hello">
+        <h1>I am a title.</h1>
+        <a> written by {{ author }}</a>
+    </div>
+</template>
+
+<script >
+    export default {
+        data () {
+            return {
+                author: "June"
+            }
+        }
+    }
+</script>
