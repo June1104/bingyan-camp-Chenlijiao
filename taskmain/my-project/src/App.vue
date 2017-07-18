144ded73285774b40f863a63f6449200c226a968
June <1175421906qq.com>
2017-07-18 23:52:18 +0800

    begin the maintask

---
+++ taskmain/my-project/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <v-header></v-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import header from './components/header/header.vue'
+  export default {
+    name: 'app',
+    components: {
+        'v-header': header
+    }
+  }
+
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ taskmain/my-project/src/components/header/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <div class="self-introduction">
+      <div class="avatar">
+        <img src="./a.jpg">
+      </div>
+    </div>
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {}
+</script>
+<style>
+  .header {
+    width: 100%;
+    height: 180px;
+    background: skyblue;
+  }
+  .self-introduction img {
+    width: 100px;
+    height: auto;
+  }
+</style>
